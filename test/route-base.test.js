bcde9acd6fc8eefcdb6bb8791dff9a2ac9b225d0
François Michaud <fmichaud@9fevrier.com>
2018-02-22 11:58:34 +0100

    Initial import

--- test/route-base.test.js
+++ test/route-base.test.js
@@ -6,3 +6,36 @@
  * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
  *
  */
+
+
+/* eslint-env mocha */
+
+require('should')
+
+const {RouteBase} = require('../lib/route-base')
+
+describe('Class {RouteBase}', () => {
+  it('should be a function', () => {
+    (RouteBase).should.be.a.Function()
+  })
+})
+
+describe('Create a instance of {RouteBase} using <new> keyword', () => {
+  it('should be an object', () => {
+    (new RouteBase()).should.be.an.Object()
+  })
+
+  it('should be an instance of {RouteBase}', () => {
+    (new RouteBase()).should.be.an.instanceOf(RouteBase)
+  })
+})
+
+describe('Create a instance of {RouteBase} using <factory> method', () => {
+  it('should be an object', () => {
+    (RouteBase.factory()).should.be.an.Object()
+  })
+
+  it('should be an instance of {PigalleBaseClass}', () => {
+    (RouteBase.factory()).should.be.an.instanceOf(RouteBase)
+  })
+})
